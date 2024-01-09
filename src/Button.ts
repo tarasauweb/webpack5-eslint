eade4daab3eeffabee7ef78047bb0058e50205a9
Artur Tarasau <tarasov.a.web@gmail.com>
2024-01-09 23:10:58 +0300

    feat:options eslint

--- src/Button.ts
+++ src/Button.ts
@@ -1,12 +1,12 @@
 import styles from './Button.module.scss';
 class Button {
-  body:HTMLElement;
-  constructor (body:HTMLElement) {
+  body: HTMLElement;
+  constructor(body: HTMLElement) {
     this.body = body;
-    this.body.insertAdjacentElement('afterbegin' , this.createButton());
+    this.body.insertAdjacentElement('afterbegin', this.createButton());
   }
 
-  private createButton () : HTMLElement {
+  private createButton(): HTMLElement {
     const btn = document.createElement('button');
     btn.classList.add(`${styles.button}`);
     btn.textContent = 'Submit';
