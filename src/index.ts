6c0c27eef2017af068145884056cef3f93b6d1dd
Artur Tarasau <tarasov.a.web@gmail.com>
2024-01-10 23:23:47 +0300

    del:test code index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
 import Button from './Button';
 
 const button = new Button(document.body);
-
-interface IObjName {
-  name : string
-}
-
-const obj : IObjName = {
-  name: 'Artur',
-};
-
-const { name } = obj;
-console.log(name);
