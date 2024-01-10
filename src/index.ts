9a5422300f641e4bff238c7ec9174436dcedd185
Artur Tarasau <tarasov.a.web@gmail.com>
2024-01-10 23:20:39 +0300

    fix:ignoreUrls

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import Button from './Button';
 
 const button = new Button(document.body);
+
+interface IObjName {
+  name : string
+}
+
+const obj : IObjName = {
+  name: 'Artur',
+};
+
+const { name } = obj;
+console.log(name);
